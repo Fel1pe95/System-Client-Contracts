473131fc3fe6b56cd8beaa4f209a035d498fc0be
Luis felipe <84816784+Fel1pe95@users.noreply.github.com>
2021-06-27 17:03:47 -0300

    Insert method

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import model.dao.ContractsDao;
 import model.dao.DaoFactory;
@@ -10,13 +11,18 @@ public class Test {
 
 	public static void main(String[] args) throws ParseException {
 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		/* encontra um contrato pelo seu endereço ID */
 		ContractsDao contracts = DaoFactory.createContractsJDBC();
 		Contracts contract = contracts.findById(1234);
 		System.out.println(contract);
 
 		/*Deleta um contrato pelo seu endereço ID*/
-		contracts.removeById(12345);
+		contracts.removeById(1357425);
+		
+		/*Inere um contrato no banco de dados*/
+		//contracts.insert(new Contracts(1357425,sdf.parse("28/02/2021") , sdf.parse("03/03/2021"), 350.50));
+		
 	}
 
 }
--- src/model/dao/impl/ContractsJDBC.java
+++ src/model/dao/impl/ContractsJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DBException;
 import model.dao.ContractsDao;
@@ -19,7 +20,24 @@ public class ContractsJDBC implements ContractsDao {
 
 	@Override
 	public void insert(Contracts obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTo contracts  (idContracts, initialDate, finalDate, totalValue) VALUES (?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			st.setInt(1, obj.getContractId());
+			st.setDate(2, new java.sql.Date(obj.getInitialDate().getTime()));
+			st.setDate(3, new java.sql.Date(obj.getFinalDate().getTime()));
+			st.setDouble(4, obj.getTotalValue());
+
+			int rowsaffecteds = st.executeUpdate();
+			
+			if (rowsaffecteds > 0) {
+				System.out.println("rows affecteds: " + rowsaffecteds);
+			}
+		} catch (SQLException e) {
+			throw new DBException("unexpected Error! no rows Affected!");
+		}
 
 	}
 
@@ -36,9 +54,9 @@ public class ContractsJDBC implements ContractsDao {
 			st = conn.prepareStatement("DELETE FROM contracts WHERE idContracts = ?");
 			st.setInt(1, Id);
 			int rows = st.executeUpdate();
-			if(rows > 0) {
+			if (rows > 0) {
 				System.out.println("Rows affected: " + rows);
-			}else {
+			} else {
 				System.out.println("no lines affected!");
 			}
 		} catch (SQLException e) {
