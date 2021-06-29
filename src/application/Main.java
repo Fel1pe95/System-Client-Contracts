27985bd773e40a611d58283f8b24adb5564db1e7
Luis felipe <84816784+Fel1pe95@users.noreply.github.com>
2021-06-28 21:14:18 -0300

    MainView.FXML & MainViewController

---
+++ src/application/Main.java
@@ -0,0 +1,37 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	private static Scene mainScene;
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			AnchorPane anchor = loader.load();
+			
+			mainScene = new Scene(anchor);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Sample javaFX application");
+			primaryStage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/gui/MainViewControler.java
@@ -0,0 +1,34 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MainViewControler {
+	
+	@FXML
+	private Button buttonRegister;
+	@FXML
+	private Button buttonNewContract;
+	@FXML
+	private Button buttonClientData;
+	@FXML
+	private Button buttonContractUpdate;
+	@FXML
+	private Button buttonCloseApp;
+	@FXML
+	private Label labelInformation;
+	
+	@FXML
+	public void onButtonRegisterAction() {}
+	@FXML
+	public void onButtonNewContractAction() {}
+	@FXML
+	public void onButtonClientDataAction() {}
+	@FXML
+	public void onButtonContractUpdateAction() {}
+	@FXML
+	public void onButtonCloseAppAction() {}
+	
+
+}
